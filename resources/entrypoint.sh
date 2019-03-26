8a0d9cf5a50e02847dd006b92985893d84be163e
Alessandro Bellanda <a.bellanda@esconsulting.it>
2019-03-26 14:50:48 +0100

    Versione 1

--- resources/entrypoint.sh
+++ resources/entrypoint.sh
@@ -4,6 +4,8 @@ mv -f /resources/vhost.conf /etc/nginx/conf.d/default.conf
 
 sed -i -e "s/PHPFPM_HOST/$PHPFPM_HOST/g" /etc/nginx/conf.d/default.conf
 sed -i -e "s/PHPFPM_PORT/$PHPFPM_PORT/g" /etc/nginx/conf.d/default.conf
+sed -i -e "s/SERVER_NAME/$VIRTUAL_HOST/" /etc/nginx/conf.d/default.conf
+sed -i -e "s/FASTCGI_APP_ENV/$FASTCGI_APP_ENV/g" /etc/nginx/conf.d/default.conf
 sed -i -e "s/CLIENT_MAX_BODY_SIZE/$CLIENT_MAX_BODY_SIZE/g" /etc/nginx/conf.d/default.conf
 
 nginx -g 'daemon off;'
